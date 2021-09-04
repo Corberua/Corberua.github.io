cd835b7ddb65a2b99fbec647497137a792f3cb50
Corberua <88579429+Corberua@users.noreply.github.com>
2021-09-04 19:25:35 +0300

    Add files via upload

---
+++ cor/js/myscript.js
@@ -0,0 +1,31 @@
+"use strict"
+
+alert("тип сайта, 1-визитка, 2-витрина")
+let a = parseInt (prompt("тип сайта", "1, 2"));
+/*let x = parseInt (a = 1);
+if(x === 1){alert ("5456");}
+else if(x != 1){alert ("546");}*/
+
+
+alert("дизайн, 3шаблон, 4уник")
+let b = parseInt (prompt("дизайн", "3, 4"));
+
+alert("адаптивность, 5-визитка, 6-витрина");
+let c = parseInt (prompt("адативность", "5, 6"));
+
+let d = a + b + c
+alert(d)
+    /* if(d == 9) alert(1000);
+     if(d == 10) alert(2000);
+     if(d == 11)alert(3000);
+     if(d == 12)alert(4000);
+     if(d == 13)alert(5000);
+     if(d == 14)alert(6000);
+     if(d == 15)alert(7000);
+     if(d == 16)alert(8000);
+     if(d == 17)alert(9000);
+     if(d == 18)alert(10000);
+    */
+    
+
+
