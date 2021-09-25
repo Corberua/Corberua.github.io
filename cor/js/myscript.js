c1518e64fa39ded5212d5d88360ce87c9f35e1f2
Corberua <88579429+Corberua@users.noreply.github.com>
2021-09-25 12:20:13 +0300

    Add files via upload

--- cor/js/myscript.js
+++ cor/js/myscript.js
@@ -6,7 +6,7 @@ elements.each ((i,el) => {
    observer.observe(el); 
 });
     
-});
+
 
 function onEntry (entry){
     entry.forEach(change => {
@@ -30,21 +30,31 @@ function calc(){
 
 setTimeout(exampleModal, 1000);
 
-
-$(document).ready(function() {
-  $('.image-link').magnificPopup({type:'image'});
+    $('.k1').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true     
 });
+    $('.image-link').magnificPopup({type:'image'});
+/*$('.k1').slick();
+$(document).ready(function() {
+    
+  $('.image-link').magnificPopup({type:'image'});
+
+});*/
 
 
 	$(function() {
-		var target_block = $(".klient");
+		var target_block = $(".stata");
 		var blockStatus = true;
 		$(window).scroll(function() {
 			var scrollEvent = ($(window).scrollTop() > (target_block.position().top - $(window).height()));
 			if(scrollEvent && blockStatus) {
 				blockStatus = false;
 				$({numberValue: 0}).animate({numberValue: 1000}, {
-					duration: 20000,
+					duration: 10000,
 					easing: "linear",
 					step: function(val) {
 						$(".klient h3").html(Math.ceil(val)); 
@@ -90,72 +100,27 @@ $('select.listen').change(function(){
     }
 })
 
+$("#inputTel").mask("+7(999) 999-9999");
+$("form").submit(function(){
+    if ($("#inputTel").val() == "" || $("#inputEmail3").val() == ""){
+        event.preventDefault();
+        alert("введите телефон");
+    }
+});
 
+   $('form').submit(function(event){
+       event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "php/mail.php",
+            data: $(this).serialize() 
+        }).done(function(){
+            $(this).find("input").val("");
+            alert("Успешно отправлено");
+            $("form").trigger("reset");
+        });
+        return false;
+    });
 
-/*}else if(a === "Визитка" && b === "Шаблон" && c === "Резина"){
-    alert("10 дней");
-    alert("3000рублей");
-}else if(a === "Визитка" && b === "Уник" && c === "Пиксель"){
-    alert("10 дней");
-    alert("3000рублей");
-}else if(a === "Визитка" && b === "Уник" && c === "Резина"){
-    alert("15 дней");
-    alert("5000рублей");  
-}else if(a === "Витрина" && b === "Шаблон" && c === "Пиксель"){
-    alert("10 дней");
-    alert("3000рублей");
-}else if(a === "Витрина" && b === "Шаблон" && c === "Резина"){
-    alert("18 дней");
-    alert("5000рублей");
-}else if(a === "Витрина" && b === "Уник" && c === "Пиксель"){
-    alert("15 дней");
-    alert("5000рублей");
-}else if(a === "Витрина" && b === "Уник" && c === "Резина"){
-    alert("20 дней");
-    alert("10000рублей");
-}
-
-/*
-let a =  (prompt("тип сайта", "Визитка, Витрина"));
-let b =  (prompt("дизайн", "Шаблон, Уник"));
-let c =  (prompt("адативность", "Пиксель, Резина"));
-if(a === "Визитка" && b === "Шаблон" && c === "Пиксель"){
-    alert("1 неделя");
-    alert("1000рублей");
-}else if(a === "Визитка" && b === "Шаблон" && c === "Резина"){
-    alert("10 дней");
-    alert("3000рублей");
-}else if(a === "Визитка" && b === "Уник" && c === "Пиксель"){
-    alert("10 дней");
-    alert("3000рублей");
-}else if(a === "Визитка" && b === "Уник" && c === "Резина"){
-    alert("15 дней");
-    alert("5000рублей");  
-}else if(a === "Витрина" && b === "Шаблон" && c === "Пиксель"){
-    alert("10 дней");
-    alert("3000рублей");
-}else if(a === "Витрина" && b === "Шаблон" && c === "Резина"){
-    alert("18 дней");
-    alert("5000рублей");
-}else if(a === "Витрина" && b === "Уник" && c === "Пиксель"){
-    alert("15 дней");
-    alert("5000рублей");
-}else if(a === "Витрина" && b === "Уник" && c === "Резина"){
-    alert("20 дней");
-    alert("10000рублей");
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
+});
 
---
+++ cor/php/test.php
@@ -0,0 +1,8 @@
+<?php
+
+$email = $_POST['user_email'];
+$phone = $_POST['user_phone'];
+
+echo "скрипт <br>". $email . "<br>" . $phone;
+
+?>
